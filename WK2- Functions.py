84d36f17b1ce921994f534613da8de07b368abaa
Richard Kim <rk.sungjin@gmail.com>
2020-08-31 16:46:07 -1000

    Update WK2- Functions.py

--- WK2- Functions.py
+++ WK2- Functions.py
@@ -41,3 +41,15 @@ def order_numbers(number1, number2):
 #    of the function call
 smaller, bigger = order_numbers(100, 99)
 print(smaller, bigger)
+
+
+#Question 4
+#Let's revisit our lucky_number function. We want to change it, so that instead of printing the message, it returns the message. This way, the calling line can print the message, or do something else with it if needed. Fill in the blanks to complete the code to make it work.
+
+def lucky_number(name):
+  number = len(name) * 9
+  return "Hello " + name + ". Your lucky number is " + str(number)
+  ___
+	    
+print(lucky_number("Kay"))
+print(lucky_number("Cameron"))
