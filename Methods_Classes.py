23d1d8d3a866c88cf2b5dc1c95778d7ba17b17e8
Richard Kim <rk.sungjin@gmail.com>
2020-09-08 20:30:42 -1000

    Update Methods_Classes.py

--- Methods_Classes.py
+++ Methods_Classes.py
@@ -18,3 +18,28 @@ class Elevator:
             self.current = floor
 
 elevator = Elevator(-1, 10, 0)
+
+elevator.up() 
+elevator.current #should output 1
+
+elevator.down() 
+elevator.current #should output 0
+
+elevator.go_to(10) 
+elevator.current #should output 10
+
+# Go to the top floor. Try to go up, it should stay. Then go down.
+elevator.go_to(10)
+elevator.up()
+elevator.down()
+print(elevator.current) # should be 9
+# Go to the bottom floor. Try to go down, it should stay. Then go up.
+elevator.go_to(-1)
+elevator.down()
+elevator.down()
+elevator.up()
+elevator.up()
+print(elevator.current) # should be 1
+
+elevator.go_to(5)
+print(elevator)
