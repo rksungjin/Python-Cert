2b8acd8ccdba8aa9b6c26bf4cf456787d06ad248
Richard Kim <rk.sungjin@gmail.com>
2020-09-24 10:15:41 -1000

    WK4

--- WK1_C2_PythonScripts.py
+++ WK1_C2_PythonScripts.py
@@ -20,18 +20,3 @@ elif check_localhost() and check_connectivity():
 else:
     print("Network checks failed")
 
-#network.py
-#!/usr/bin/env python3
-import requests
-import socket
-def check_localhost():
-    localhost = socket.gethostbyname('localhost')
-    if localhost == '127.0.0.1':
-        return True
-    return False
-def check_connectivity():
-    request = requests.get('http://www.google.com')
-    response = request.status_code
-    if response == 200:
-        return True
-    return False
