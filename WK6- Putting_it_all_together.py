6f893b47ca0970876f00b1d62a08d416a5c0d067
Richard Kim <rk.sungjin@gmail.com>
2020-09-28 23:49:00 -1000

    Create WK6- Putting_it_all_together.py

---
+++ WK6- Putting_it_all_together.py
@@ -0,0 +1,29 @@
+def get_event_date(event):
+  return event.date
+
+def current_users(events):
+  events.sort(key=get_event_date)
+  machines = {}
+  for event in events:
+    if event.machine not in machines:
+      machines[event.machine] = set()
+      #add set for users 
+    if event.type == "login":
+      machines[event.machine].add(event.user)
+    elif event.type == "logout" and event.user in machines[event.machine]:
+      machines[event.machine].remove(event.user)
+  return machines
+
+def generate_report(machines):
+  for machine, users in machines.items():
+    if len(users) > 0:
+      user_list = ", ".join(users)
+      print("{}: {}".format(machine, user_list))
+
+
+class Event:
+  def __init__(self, event_date, event_type, machine_name, user):
+    self.date = event_date
+    self.type = event_type
+    self.machine = machine_name
+    self.user = user
